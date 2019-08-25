be66d20ce0fb7a87cc3cf346fbe35fa5c75a3c40
ankitasood <ankita.s33@gmail.com>
2019-08-25 15:59:31 -0500

    final issues

---
+++ src/app/test/test-mock.module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from '@angular/core';
+import {MockHeaderComponent} from './mock-header.component';
+
+@NgModule({
+  declarations: [MockHeaderComponent]
+})
+
+export class TestMockModule {}
